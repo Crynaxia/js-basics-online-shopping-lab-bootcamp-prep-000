3b6d3780682112c930d35093f00424e22a3fe664
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-06-15 21:08:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-var price = Math.ceil(Math.random() * 100);
-cart.push({ itemName: item, itemPrice: price })
-console.log (`${item} has been added to your cart.`)
-return cart;
+  var price = Math.ceil(Math.random() * 100);
+  cart.push({ itemName: item, itemPrice: price })
+  console.log (`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
