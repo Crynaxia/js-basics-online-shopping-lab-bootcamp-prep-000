72d4622d6bcc4d2af5837d8702536266b98c2431
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-06-15 22:17:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   var tempCart = cart;
-  cart = [];
-  return 'Your total cost is $'+ total() +', which will be charged to the card ' + cardNumber +  '.'
+ 
+  var result = 'Your total cost is $'+ total() +', which will be charged to the card ' + cardNumber +  '.';
+   cart = [];
+   return result;
 }
